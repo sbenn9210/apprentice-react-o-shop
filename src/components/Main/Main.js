cc4307f6bbc9a3615f7c8ef5dcbefeb7badaf4c7
Shawn Benny <shawnbenny@Shawns-MacBook-Pro.local>
2020-08-12 16:28:14 -0500

    feat(o-shop): Add updated routes for main and app js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,16 +11,18 @@ import { Route, Switch } from 'react-router';
 
 function Main() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/products" component={Products}></Route>
-      <Route exact path="/adminorders" component={AdminOrders}></Route>
-      <Route exact path="/adminproducts" component={AdminProducts}></Route>
-      <Route exact path="/checkout" component={Checkout}></Route>
-      <Route exact path="/orders" component={Orders}></Route>
-      <Route exact path="/ordersuccess" component={OrderSuccess}></Route>
-      <Route exact path="/shoppingcart" component={Shoppingcart}></Route>
-    </Switch>
+    <div className="main">
+      <Switch>
+        <Route exact path="/" component={Products}></Route>
+        <Route exact path="/products" component={Products}></Route>
+        <Route exact path="/admin-orders" component={AdminOrders}></Route>
+        <Route exact path="/admin-products" component={AdminProducts}></Route>
+        <Route exact path="/checkout" component={Checkout}></Route>
+        <Route exact path="/orders" component={Orders}></Route>
+        <Route exact path="/ordersuccess" component={OrderSuccess}></Route>
+        <Route exact path="/cart" component={Shoppingcart}></Route>
+      </Switch>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import Main from '../components/Main/Main';
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main>
+      <Header />
+      <Main />
+    </main>
+  );
 }
 
 export default Home;
